8efb491ec8dca18f5b9ae73093cbf430270dfd07
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-08-15 07:15:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'down-id.img.susercontent.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
