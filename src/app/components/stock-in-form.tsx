f73d7953474bc4bfab53a8bdfe4b762c3b6fe21c
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-08-16 12:42:29 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button type="butt

--- src/app/components/stock-in-form.tsx
+++ src/app/components/stock-in-form.tsx
@@ -400,7 +400,7 @@ export function StockInForm({
                                                             )}
                                                         />
                                                         <Button type="button" variant="outline" size="sm" onClick={() => applyMasterQuantity(parentName)}>
-                                                            {t.bulkStockInDialog.applyToAll}
+                                                            {t.common.apply}
                                                         </Button>
                                                     </div>
                                                 </TableCell>
