ed945ce7f64ad6486ac76c9698e8fc3bd640a719
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-08-17 13:51:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent>
    
    wit

--- src/app/finance/assets/page.tsx
+++ src/app/finance/assets/page.tsx
@@ -201,7 +201,7 @@ export default function AssetReportPage() {
                         <CardTitle>{TAsset.chartTitle}</CardTitle>
                     </CardHeader>
                     <CardContent>
-                       <ChartContainer config={chartConfig} className="min-h-72 w-full">
+                       <ChartContainer config={chartConfig} className="min-h-64 w-full">
                             <LineChart accessibilityLayer data={chartData} margin={{ top: 20, left: 12, right: 12 }}>
                                 <CartesianGrid vertical={false} />
                                 <XAxis
