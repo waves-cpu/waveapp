94c54c13f23863b532f2f28b1396aadca59bb090
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-08-26 13:47:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/sales/reseller/history/page.tsx
+++ src/app/sales/reseller/history/page.tsx
@@ -26,6 +26,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { useToast } from '@/hooks/use-toast';
 import Link from 'next/link';
