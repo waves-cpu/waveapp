9204ba26df41863c4a62c3a48296b78f8e47bd6a
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-08-28 14:25:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/shipping/receipt/page.tsx
+++ src/app/shipping/receipt/page.tsx
@@ -29,6 +29,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Input } from '@/components/ui/input';
 import { Checkbox } from '@/components/ui/checkbox';
