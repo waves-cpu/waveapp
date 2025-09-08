25f628de4ae63f217aa9983f71a5b099330c6c84
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-09-08 11:08:37 +0000

    Ini kenapa tidak bisa bikin invoice pdf ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eBodNovCsvsQA5LTDy7iM4haeF6FcA5g==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
       </head>
       <body className="font-body antialiased h-full">
         <ThemeProvider
