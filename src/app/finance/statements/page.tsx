7640d0dd552e206af44360cd8e34b18dadf76a05
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-11-03 14:27:11 +0000

    Hapus text nilai  (_for element <CardTitle>_)

--- src/app/finance/statements/page.tsx
+++ src/app/finance/statements/page.tsx
@@ -626,7 +626,7 @@ export default function SalesReportPage() {
                     </Card>
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">Nilai Pembatalan</CardTitle>
+                            <CardTitle className="text-sm font-medium">Pembatalan</CardTitle>
                             <Ban className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
