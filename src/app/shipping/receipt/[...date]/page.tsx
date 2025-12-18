994fbd83023653c260dc1de485793a7846cf5fda
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-12-18 14:09:45 +0000

    Remove tombol scan resi  (_for element <LinkComponent>_)

--- src/app/shipping/receipt/[...date]/page.tsx
+++ src/app/shipping/receipt/[...date]/page.tsx
@@ -279,12 +279,6 @@ export default function ReceiptPage() {
                             />
                             </PopoverContent>
                         </Popover>
-                         <Button size="sm" asChild>
-                             <Link href="/mobile">
-                                <ScanLine className="mr-2 h-4 w-4" />
-                                Scan Resi
-                             </Link>
-                         </Button>
                          {selectedIds.size > 0 && (
                             <Button size="sm" onClick={handleProcessShipment} disabled={isProcessing}>
                                 <Send className="mr-2 h-4 w-4" />
@@ -474,4 +468,5 @@ export default function ReceiptPage() {
             />
         </AppLayout>
     );
-}
+
+    
