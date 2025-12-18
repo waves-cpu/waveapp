ae45eb46172e4aaecfe57dcc3029a139573da79d
Waveblast Club Team <waveblastclub.team@gmail.com>
2025-12-18 12:51:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -31,9 +31,7 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  experimental: {
-    serverComponentsExternalPackages: ['better-sqlite3'],
-  },
+  serverExternalPackages: ['better-sqlite3'],
 };
 
 export default nextConfig;
