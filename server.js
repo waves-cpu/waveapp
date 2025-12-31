ff84d87ff8d20a5ed4b9c8ce8a4ffdddb8a43676
Sativa <ahmadsadami12@gmail.com>
2025-12-31 10:37:23 +0000

    Sama saja coba perbaiki bagaimana cara nya agar ke mode produksi ?

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const next = require('next');
 const fs = require('fs');
 const path = require('path');
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
