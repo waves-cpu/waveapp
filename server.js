a68cf9b816c313cac991cd80d135b43260ad4914
Sativa <ahmadsadami12@gmail.com>
2025-12-31 11:25:16 +0000

    npm error Missing script: "start:prod" npm error npm error To see a list

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const next = require('next');
 const fs = require('fs');
 const path = require('path');
 
+// Force production mode
 const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
