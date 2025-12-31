687fb58004a2456f2c772f571903798d47c5357c
Sativa <ahmadsadami12@gmail.com>
2025-12-31 10:44:19 +0000

    Sama saja

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV !== 'production',
 });
 
 const nextConfig: NextConfig = {
