8cae152451a18a6dfdbda69d3a071eff06dad9e5
Waveblast Club Team <waveblastclub.team@gmail.com>
2026-01-20 08:35:23 +0000

    Ubah warna jadi warna hitam  (_for element span_)

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -3,15 +3,16 @@ import Image from 'next/image';
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2 font-bold text-xl text-primary">
+    <div className="flex items-center gap-2 font-bold text-xl text-foreground">
       <Image
-        src="/basic.png"
+        src="/icons/basic.png"
         alt="Waveblast Logo"
         width={32}
         height={32}
-        className="h-8 w-8"
-      />
-      <span className="font-headline">Waveblast</span>
+        className="h-8 w-8" 
+        />
+        <span className="font-headline">Waveblast</span>
     </div>
   );
+  
 }
