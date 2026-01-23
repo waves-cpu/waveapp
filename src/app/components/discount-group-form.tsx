ee14ffe1fd3f8cf403a2abac863b64e6aaa7292d
Waveblast Club Team <waveblastclub.team@gmail.com>
2026-01-23 08:52:15 +0000

    Bagus taoi sedikit lagi aja di kedalemin untuk iconnya dari ini, sedikit

--- src/app/components/discount-group-form.tsx
+++ src/app/components/discount-group-form.tsx
@@ -521,7 +521,7 @@ export function DiscountGroupForm({ existingGroup, isVoucherForm }: DiscountGrou
                                                         return (
                                                             <TableRow key={field.variantId}>
                                                                 <TableCell>
-                                                                    <div className="flex items-center gap-3">
+                                                                    <div className="flex items-center gap-3 pl-4">
                                                                         <div className="flex h-8 w-8 items-center justify-center rounded-sm shrink-0">
                                                                             <Store className="h-5 w-5 text-gray-400" />
                                                                         </div>
@@ -598,3 +598,4 @@ export function DiscountGroupForm({ existingGroup, isVoucherForm }: DiscountGrou
         </>
     );
 }
+
